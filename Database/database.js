7181e2724387d4430fec251b578422ee98a26e87
Rahul Lingwal <rahullingwal200@gmail.com>
2026-08-01 22:53:32 +0530

    Refactor database.js to set up Express server and user API endpoints

--- Database/database.js
+++ Database/database.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import express from "express";
 
-//Connection
+const app = express();
+
+//DB Connection
 mongoose
   .connect("mongodb://localhost:27017/user-info")
   .then(() => console.log("MongoDB connected"))
@@ -34,9 +37,41 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model("User", userSchema);
 
-User.create({
-  username: "Rahul Lingwal",
-  email: "rahul@gmail.com",
-  city: "Dehradun",
-  state: "Uttarakhan",
+// User.create({
+//   username: "Rahul Lingwal",
+//   email: "rahul@gmail.com",
+//   city: "Dehradun",
+//   state: "Uttarakhan",
+// });
+
+const port = 5000;
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/users", async (req, res) => {
+  const body = req.body;
+
+  if (!body || !body.username || !body.email || !body.city || !body.state) {
+    return res.status(400).json({ msg: "All fields are required." });
+  }
+
+  const result = await User.create({
+    username: body.username,
+    email: body.email,
+    city: body.city,
+    state: body.state,
+  });
+
+  console.log("Result : ", result);
+
+  return res.status(201).json({ msg: "success" });
+});
+
+app.get("/api/users", async (req, res) => {
+  const allUsers = await User.find({});
+  console.log(allUsers);
+  return res.json(allUsers);
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
